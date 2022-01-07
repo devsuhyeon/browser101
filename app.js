1e53286dd49e0f99d0c973ce10fe11be2f80a9ee
suhyeooon <93829156+suhyeooon@users.noreply.github.com>
2022-01-07 22:43:27 +0900

    JS: Check button toggle + delete button

---
+++ app.js
@@ -0,0 +1,71 @@
+const addBtn = document.querySelector('.footer__button');
+const input = document.querySelector('.footer__input');
+const items = document.querySelector('.items');
+
+function onAdd() {
+  const text = input.value;
+  if (!text) {
+    input.focus();
+    return;
+  }
+
+  const item = createItem(text);
+  items.append(item);
+
+  item.scrollIntoView({ block: 'center' });
+
+  input.value = '';
+  input.focus();
+}
+
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
+
+  const itemInfo = document.createElement('div');
+  itemInfo.setAttribute('class', 'item__info');
+
+  const checkBtn = document.createElement('button');
+  checkBtn.setAttribute('class', 'item__check');
+  checkBtn.innerHTML = `<i class="far fa-check-circle"></i>`;
+  checkBtn.addEventListener('click', () => {
+    checkBtn.classList.toggle('checked');
+    itemName.classList.toggle('checked');
+  });
+
+  const itemName = document.createElement('span');
+  itemName.setAttribute('class', 'item__name');
+  itemName.innerText = text;
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'item__delete');
+  deleteBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+  deleteBtn.addEventListener('click', () => {
+    items.removeChild(itemRow);
+  });
+
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item__divider');
+
+  itemInfo.append(checkBtn);
+  itemInfo.append(itemName);
+  item.append(itemInfo);
+  item.append(deleteBtn);
+  itemRow.append(item);
+  itemRow.append(itemDivider);
+
+  return itemRow;
+}
+
+addBtn.addEventListener('click', () => {
+  onAdd();
+});
+
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    onAdd();
+  }
+});
