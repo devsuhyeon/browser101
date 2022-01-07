84fddf15cc1c92bc8c1ee3acaf881553fa5f631b
suhyeooon <93829156+suhyeooon@users.noreply.github.com>
2022-01-07 22:49:49 +0900

    JS: check button toggle + delete button

--- Shopping List/app.js
+++ Shopping List/app.js
@@ -1,6 +1,6 @@
-const addBtn = document.querySelector(".footer__button");
-const input = document.querySelector(".footer__input");
-const items = document.querySelector(".items");
+const addBtn = document.querySelector('.footer__button');
+const input = document.querySelector('.footer__input');
+const items = document.querySelector('.items');
 
 function onAdd() {
   const text = input.value;
@@ -12,36 +12,43 @@ function onAdd() {
   const item = createItem(text);
   items.append(item);
 
-  item.scrollIntoView({ block: "center" });
+  item.scrollIntoView({ block: 'center' });
 
-  input.value = "";
+  input.value = '';
   input.focus();
 }
 
 function createItem(text) {
-  const itemRow = document.createElement("li");
-  itemRow.setAttribute("class", "item__row");
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
 
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
 
-  const itemInfo = document.createElement("div");
-  itemInfo.setAttribute("class", "item__info");
+  const itemInfo = document.createElement('div');
+  itemInfo.setAttribute('class', 'item__info');
 
-  const checkBtn = document.createElement("button");
-  checkBtn.setAttribute("class", "item__check");
+  const checkBtn = document.createElement('button');
+  checkBtn.setAttribute('class', 'item__check');
   checkBtn.innerHTML = `<i class="far fa-check-circle"></i>`;
+  checkBtn.addEventListener('click', () => {
+    checkBtn.classList.toggle('checked');
+    itemName.classList.toggle('checked');
+  });
 
-  const itemName = document.createElement("span");
-  itemName.setAttribute("class", "item__name");
+  const itemName = document.createElement('span');
+  itemName.setAttribute('class', 'item__name');
   itemName.innerText = text;
 
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item__delete");
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'item__delete');
   deleteBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+  deleteBtn.addEventListener('click', () => {
+    items.removeChild(itemRow);
+  });
 
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item__divider");
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item__divider');
 
   itemInfo.append(checkBtn);
   itemInfo.append(itemName);
@@ -53,12 +60,12 @@ function createItem(text) {
   return itemRow;
 }
 
-addBtn.addEventListener("click", () => {
+addBtn.addEventListener('click', () => {
   onAdd();
 });
 
-input.addEventListener("keypress", (event) => {
-  if (event.key === "Enter") {
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
     onAdd();
   }
 });
