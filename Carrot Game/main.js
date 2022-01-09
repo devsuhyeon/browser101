bac1abf928dc7d65766299219894e11ff8e5fae9
suhyeooon <suhyeonlee.dev@gmail.com>
2022-01-09 14:46:06 +0900

    JS: Game start

--- Carrot Game/main.js
+++ Carrot Game/main.js
@@ -4,7 +4,55 @@ const gameField = document.querySelector('.game__field');
 const fieldRect = gameField.getBoundingClientRect();
 const fieldWidth = fieldRect.width;
 const fieldHeight = fieldRect.height;
+
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
 const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
+
+gameBtn.addEventListener('click', () => {
+    if (started) {
+        stopGame(); 
+    } else {
+        startGame();
+    }
+    started = !started;
+});
+
+function startGame() {
+    initGame();
+    showStopBtn();
+    showTimerAndScore();
+}
+
+function stopGame() {
+}
+
+function showStopBtn() {
+    const icon = gameBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
+
+function showTimerAndScore() {
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
+
+function initGame() {
+    gameField.innerHTML = '';
+    gameScore.innerText = CARROT_COUNT;
+    createItem('carrot', CARROT_COUNT);
+    createItem('bug', BUG_COUNT);
+}
 
 function randomCoords() {
     const x = Math.floor(Math.random() * (fieldWidth - CARROT_SIZE));
@@ -28,12 +76,3 @@ function createItem(itemType, count) {
         gameField.append(item);
     }
 }
-
-function initGame() {
-    const numOfCarrot = 5;
-    const numOfBug = 5;
-    createItem('carrot', numOfCarrot);
-    createItem('bug', numOfBug);
-}
-
-initGame();
